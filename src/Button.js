46b55e314109d9541359a99a8ac290929f0d61b5
Krzysztof Matyga <krzysztof.matyga@noinputsignal.com>
2023-01-17 20:18:06 +0100

    finished

---
+++ src/Button.js
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import { ThemeConsumer, ThemeContext } from './ThemeContext'
+
+
+const styles = {
+    dark: {
+        backgroundColor: "black",
+        color: "white"
+    },
+    light: {
+        backgroundColor: "white",
+        color: "black"
+    }
+}
+
+const Button = ({ children }) => {
+    const context = useContext(ThemeContext);
+    console.log(context)
+  return (
+    <div>
+        <ThemeConsumer>
+            {value => (
+            <button style={styles[value]}>
+                {children} {value}
+            </button>
+            )}
+        </ThemeConsumer>
+          <button style={styles[context]}>
+              {children} {context}
+          </button>
+    </div>
+  )
+}
+
+export default Button
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from "./Button"
+
+const Sidebar = (props) => {
+  return (
+    <div>
+      <Button>ThemedButton</Button>
+    </div>
+  )
+}
+
+export default Sidebar
